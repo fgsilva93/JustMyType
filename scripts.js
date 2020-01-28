e6f513cc10b6a9f673f9b85532a53b8d55dc1941
IB_Student <student@InnovateBhams-MacBook-Pro.local>
2020-01-28 16:55:46 -0600

    new commit

--- scripts.js
+++ scripts.js
@@ -1,6 +1,18 @@
 let $upperkeys = $('#keyboard-upper-container'); 
+let $lowerKeys = $('#keyboard-lower-container');
 
+// can delete the click function, it will still work. 
 ($upperkeys).click(function() {
     $('#keyboard-upper-container').hide();
 });
 
+($lowerKeys).keydown(function() {
+    if(which === 16) {
+        $(upperKeys).show();
+        $(lowerKeys).hide();
+    }
+})
+
+/*($upperKeys).keyup(function () {
+
+})*/
