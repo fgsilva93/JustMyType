b6c4259f3dedd7c93a4f56f143d49c9dc2c7ccc3
fgsilva93 <fgsilva93@gmail.com>
2020-01-29 00:20:36 -0600

    commit

--- scripts.js
+++ scripts.js
@@ -1,18 +1,26 @@
+$(document).ready(function() {
+
+})
+
 let $upperkeys = $('#keyboard-upper-container'); 
 let $lowerKeys = $('#keyboard-lower-container');
 
 // can delete the click function, it will still work. 
-($upperkeys).click(function() {
+//($upperkeys).click(function() {
     $('#keyboard-upper-container').hide();
+//});
+
+$(document).keydown(function(shift) {
+    //which is use to return a unicode character of a key and 16 is the  key code of shift
+    if(shift.keyCode === 16) { 
+        ($lowerKeys).hide();
+        ($upperkeys).show();
+    }
 });
 
-($lowerKeys).keydown(function() {
-    if(which === 16) {
-        $(upperKeys).show();
-        $(lowerKeys).hide();
+$(document).keyup(function (shift) {
+    if(shift.keyCode === 16) {
+        ($upperkeys).hide();
+        ($lowerKeys).show(); 
     }
-})
-
-/*($upperKeys).keyup(function () {
-
-})*/
+});
